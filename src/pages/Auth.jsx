5e02061d548bafd81eecafebbd64be85cdbb8f91
Muhammad Tahir <mtahir.kcsf19@iba-suk.edu.pk>
2024-04-30 08:29:58 +0500

    update Auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -24,20 +24,8 @@ const Login = () => {
               Telently
             </Link>
             <Outlet />
-            {/* <div class="flex flex-col pt-10 sm:pt-20">
-              <h2 class="font-bold text-3xl sm:text-[40px] text-[#111827] max-w-[410px] mx-auto leading-[130%] tracking-[-1.4px] text-center mb-[18px]">
-                Log in to Talently.ai
-              </h2>
-              <p class="text-[#445E7C] text-center text-base max-w-[420px] mx-auto leading-[150%] mb-6">
-                Enter your account credentials
-              </p>
-              <LoginForm />
-
-
-            </div> */}
           </div>
         </div>
-
         <div class="flex-grow hidden laptopSm:flex bg-signup bg-no-repeat bg-cover w-full max-w-[422px]"></div>
       </div>
     </div>
